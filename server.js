bed2528ce2048f289eeba321e11de8395384abb3
reza.h <rezarayan78@gmail.com>
2024-02-26 11:21:01 +0330

    Initial Structure of Platform added

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const config = require("./modules/config");
+global.config = require("./modules/config");
+
+const app = express();
+
+// Platform Configurations
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ type: "application/json" }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("Home Page API");
+});
+
+app.listen(config.port, () =>
+  console.log(`Server is Running on port :${config.port}`),
+);
