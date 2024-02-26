e280b510b54f7416c84dacebdc4f9a7972dc3133
reza.h <rezarayan78@gmail.com>
2024-02-26 14:23:13 +0330

    Signup controller changed to class

--- modules/controllers/v1/usersControllers.js
+++ modules/controllers/v1/usersControllers.js
@@ -1,29 +1,33 @@
 const User = require("../../models/user");
 
-exports.signup = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const newUser = new User({ username, email, password });
+class UsersControllers {
+  async signup(req, res) {
+    try {
+      const { username, email, password } = req.body;
 
-    // Check User Exist
-    const existingUser = await User.find({ email });
-    if (existingUser) {
-      return res.status(401).json({
-        success: false,
-        message: "این ایمیل حساب  دارد",
+      const newUser = new User({ username, email, password });
+
+      // Check User Exist
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(401).json({
+          success: false,
+          message: "این ایمیل حساب  دارد",
+        });
+      }
+      const user = await newUser.save();
+      return res.status(200).json({
+        success: true,
+        message: "ثبت نام موفقیت آمیز بود",
+        user,
       });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ success: false, message: "There is an Error in Server" });
     }
-
-    const user = await newUser.save();
-    return res.status(200).json({
-      success: true,
-      message: "ثبت نام موفقیت آمیز بود",
-      user,
-    });
-  } catch (error) {
-    console.log(error);
-    return res
-      .status(500)
-      .json({ success: false, message: "There is an Error in Server" });
   }
-};
+}
+
+module.exports = new UsersControllers();
--- modules/routes/api/v1/usersRoutes.js
+++ modules/routes/api/v1/usersRoutes.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 
 // Controllers
-const { signup } = require("../../../controllers/v1/usersControllers");
+const UsersControllers = require("../../../controllers/v1/usersControllers");
 
 const router = Router();
 
-router.post("/sign-up", signup);
+router.post("/sign-up", UsersControllers.signup);
 
 router.post("/login", (req, res) => {
   res.json("Login Route");
