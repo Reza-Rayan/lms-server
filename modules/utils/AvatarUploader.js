45978a2b78486f7bbea6a92bcb2d42df40f4fb17
Reza Rayan <rezarayan78@gmail.com>
2024-02-27 20:46:21 +0330

    Creation of Directories of avatars  added

--- modules/utils/AvatarUploader.js
+++ modules/utils/AvatarUploader.js
@@ -1,10 +1,35 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+
+// Create Folders of Uploads Based on Date
+const getUploadsPath = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  return path.join('uploads','avatars', year.toString(), month, day);
+};
+
+// Create Directories
+const createDirectories = (dirPath) => {
+  dirPath.split(path.sep).reduce((currentPath, folder) => {
+    currentPath += folder + path.sep;
+    if (!fs.existsSync(currentPath)) {
+      fs.mkdirSync(currentPath);
+    }
+    return currentPath;
+  }, '');
+};
+
 
 // Multer Storage Config
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Uploads folder where files will be stored
+    const uploadPath = getUploadsPath();
+    createDirectories(uploadPath);
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -17,7 +42,7 @@ const storage = multer.diskStorage({
 const fileFilter = function (req, file, cb) {
   // Accept images only
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-    return cb(new Error('Only image files are allowed!'), false);
+    return cb(new Error('فقط می توانید عکس بارگذاری کنید'), false);
   }
   cb(null, true);
 };
