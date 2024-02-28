bb36eb49fd9fc8dc156bf3fd3d650bcb71af9ed7
reza.h <rezarayan78@gmail.com>
2024-02-28 15:27:01 +0330

    User model address edited

--- modules/controllers/v1/admin/usersControllers.js
+++ modules/controllers/v1/admin/usersControllers.js
@@ -1,4 +1,4 @@
-const User = require("../../../models/user");
+const User = require("../../../models/User");
 const { validateUser } = require("../../../validations/userValidator");
 
 class UsersControllers {
