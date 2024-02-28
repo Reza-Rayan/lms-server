6cb4ca795675842cf67cb9be28c612708522db77
reza.h <rezarayan78@gmail.com>
2024-02-28 15:23:07 +0330

    Course model added

---
+++ modules/models/Course.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const timeStamps = require("mongoose-timestamp");
+
+const courseSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    min: 2,
+    length: 300,
+  },
+  description: {
+    type: String,
+    required: true,
+    min: 10,
+    max: 600,
+  },
+  imageBanner: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  teacher: {
+    type: String,
+    required: true,
+  },
+  students: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+courseSchema.plugin(timeStamps);
+
+const Course = model("Course", courseSchema);
+
+module.exports = Course;
