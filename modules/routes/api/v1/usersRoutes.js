67527f9071e256f1acba11e27090b3c20c8bba4c
reza.h <rezarayan78@gmail.com>
2024-03-09 15:11:34 +0330

    .

--- modules/routes/api/v1/usersRoutes.js
+++ modules/routes/api/v1/usersRoutes.js
@@ -3,14 +3,13 @@ const avatarUploader = require("../v1/middleware/AvatarUploader");
 
 // Controllers
 const usersControllers = require(`${config.path.controllers}/students/usersControllers`);
-const courseControllers = require(`${config.path.controllers}/students/courseControllers`)
+const courseControllers = require(`${config.path.controllers}/students/courseControllers`);
 
 const router = Router();
 
 // Courses Routes
-router.get("/courses", courseControllers.index)
-router.get("/courses/:id", courseControllers.signle)
-
+router.get("/courses", courseControllers.index);
+router.get("/courses/:id", courseControllers.signle);
 
 // User Management Routes
 router.post("/sign-up", usersControllers.signup);
@@ -21,8 +20,6 @@ router.post(
   "/upload-avatar/:id",
   avatarUploader.single("avatar"),
   usersControllers.uploadAvatar
-  );
-
-
+);
 
 module.exports = router;
