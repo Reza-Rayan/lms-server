557492b6877e2e000ad3c81e5df3245f216e666f
reza.h <rezarayan78@gmail.com>
2024-03-11 16:06:37 +0330

    student access authorization removed

--- modules/routes/api/v1/usersRoutes.js
+++ modules/routes/api/v1/usersRoutes.js
@@ -6,29 +6,18 @@ const usersControllers = require(`${config.path.controllers}/students/usersContr
 const courseControllers = require(`${config.path.controllers}/students/courseControllers`);
 
 const router = Router();
-// Auth Middleware
-const AuthMiddleware = require("./middleware/verifyToken");
 
 // Courses Routes
-router.get(
-  "/courses",
-  AuthMiddleware.verifyToken("student"),
-  courseControllers.index
-);
+router.get("/courses", courseControllers.index);
 router.get("/courses/:id", courseControllers.signle);
 
 // User Management Routes
 router.post("/sign-up", usersControllers.signup);
 router.post("/login", usersControllers.login);
 router.get("/:id", usersControllers.single);
-router.put(
-  "/:id",
-  AuthMiddleware.verifyToken("student"),
-  usersControllers.update
-);
+router.put("/:id", usersControllers.update);
 router.post(
   "/upload-avatar/:id",
-  AuthMiddleware.verifyToken("student"),
   avatarUploader.single("avatar"),
   usersControllers.uploadAvatar
 );
